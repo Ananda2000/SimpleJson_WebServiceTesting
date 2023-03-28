43be5b317242f5cc47641a26bec40726e834ef90
Ananda Ramakrishna <ramakrishnaka99@gmail.com>
2023-03-28 20:09:00 +0530

    this is commit on evening 8:08

--- src/main/java/com/Json/FileReader/A2_program.java
+++ src/main/java/com/Json/FileReader/A2_program.java
@@ -22,6 +22,7 @@ public class A2_program
 		//System.out.println(s1);
 		JsonPath jsp1 = new JsonPath(s1);
 		System.out.println(jsp1.get("Names1"));
+		System.out.println("this is latest changes on everning 8:08pm");
 	}
 
 }
