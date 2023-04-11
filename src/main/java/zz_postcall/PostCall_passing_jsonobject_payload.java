10eb8fa674f16e37e78b6a490a573a7db713da52
Ananda Ramakrishna <ramakrishnaka99@gmail.com>
2023-04-11 07:16:48 +0530

    committing on 11-april morning 7:16

--- src/main/java/zz_postcall/PostCall_passing_jsonobject_payload.java
+++ src/main/java/zz_postcall/PostCall_passing_jsonobject_payload.java
@@ -1,9 +1,13 @@
-// Here am reading the payload from #payloads/payloadessss.java files
-
 package zz_postcall;
+
+import java.io.File;
+import java.io.IOException;
 
 import org.testng.annotations.Test;
 
+import com.deserialization.pojoclasses.BookingIdPostcall_response;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import io.restassured.RestAssured;
@@ -13,17 +17,21 @@ import io.restassured.response.ResponseBody;
 import io.restassured.specification.RequestSpecification;
 import payloads.Payloadesss;
 
-public class PostCall_readStringFromExternallocation 
+public class PostCall_passing_jsonobject_payload 
 {
-@Test	
-	public void read_fromOtherClasss()
+	@Test
+	public void passingJavaobject_As_payload() throws JsonParseException, JsonMappingException, IOException
 	{
-		ObjectMapper objmappp= new ObjectMapper();
+		ObjectMapper objmap11 = new ObjectMapper();
+		File fl123 = new File("JsonFiles/postcall.json");
+		
+		BookingIdPostcall_response bookpost = objmap11.readValue(fl123,BookingIdPostcall_response.class );
 		
 		RestAssured.baseURI="https://restful-booker.herokuapp.com/booking";
 		RequestSpecification requestspec = RestAssured.given();
 		requestspec.contentType(ContentType.JSON);
-		requestspec.body(Payloadesss.CreateBooking());
+		//HERE WE ARE PASSING REQUEST BODY AS JAVA OBJECT... DESERIALIZED OBJECT..
+		requestspec.body(bookpost);
 		Response resp = requestspec.post();
 		
 		System.out.println(resp.getStatusCode());
@@ -32,6 +40,8 @@ public class PostCall_readStringFromExternallocation
 		ResponseBody respbody = resp.getBody();
 		System.out.println("The response body --------------------------");
 		System.out.println(respbody.asString());
+		
+		
 	}
 
 }
